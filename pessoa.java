72f71683a70fbc0c2cca6b740de2089f7cdf3ffc
nicolas <Nicojss1307@gmail.com>
2024-09-23 22:27:40 -0300

    formulário visual

--- pessoa.java
+++ pessoa.java
@@ -1,23 +1,85 @@
-//import javax.swing.JOptionPane;
-//import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 
-public class Pessoa{
-        private String cpf;
-        String medicamentos;
-        String sintomas;
-        String pressaoArterial;
-        String nome;
+//componentes do form
+public class Pessoa extends JFrame implements ActionListener{
+        private JLabel cpfLabel, nomeLabel, pressaoArterialLabel, sintomasLabel, medicamentosLabel; 
+        private JTextField cpfField, nomeField, pressaoArterialField, sintomasField, medicamentosField;
+        private JButton enviarButton;
+        private JTextArea resultadoArea;
 
-        //construtor
-        public Pessoa(String cpf, String medicamentos, String sintomas, String pressaoArterial, String nome) {
+
+        //Construtor para criar interface gráfica 
+        public Pessoa(){
+                //janela
+                setTitle("Formulário do paciente");
+                setSize(300, 250);
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setLayout(new FlowLayout());
+ 
+
+                //inciando componetes
+                nomeLabel= new JLabel("Nome: ");
+                cpfLabel = new JLabel("cpf: ");
+                sintomasLabel = new JLabel("sintomas: ");
+                pressaoArterialLabel= new JLabel("Pressão arterial: ");
+                medicamentosLabel = new JLabel("Medicamento: ");
+                medicamentosField = new JTextField(15);
+                nomeField = new JTextField( 15);
+                cpfField = new JTextField(15);
+                sintomasField = new JTextField( 20);
+                pressaoArterialField = new JTextField(4);
+                enviarButton = new JButton("Enviar");
+                resultadoArea = new JTextArea( 5, 20);
+
+
+                //adicionando coponetes à janela 
+                add(nomeLabel);
+                add(nomeField);
+                add(cpfLabel);
+                add(cpfField);
+                add(sintomasLabel);
+                add(sintomasField);
+                add(medicamentosLabel);
+                add(medicamentosField);
+                add(pressaoArterialLabel);
+                add(pressaoArterialField);
+                add(enviarButton);
+                add(new JScrollPane(resultadoArea));
+                //adicionar ação no botão
+                enviarButton.addActionListener(this);
+
+                //tornando visível
+                setVisible(true);
+
+        }
+        //chamado quando o botão é clicado
+         @Override
+         public void actionPerformed(ActionEvent e){
+                String cpf = cpfField.getText();
+                String medicamentos = medicamentosField.getText();
+                String sintomas = sintomasField.getText();
+                String pressaoArterial = pressaoArterialField.getText();
+                String nome = nomeField.getText();
+                resultadoArea.setText("Nome: "+ nome + "\ncpf:"+ cpf + "\nmedicamentos: " + medicamentos + "\nsintomas: "+ sintomas + "\npressaoArterial: " + pressaoArterial);
+        }
+        /*public pessoa extends Jframe (String cpf, String medicamentos, String sintomas, String pressaoArterial, String nome) {
                 this.cpf = cpf;
                 this.medicamentos = medicamentos;
                 this.sintomas = sintomas;
                 this.pressaoArterial =pressaoArterial;
                 this.nome = nome;
+        } */
+        // Método do principal para rodar o formulário
+
+        public static void main(String[] args) {
+                // Criando um objeto da classe MinhaClasse
+                new Pessoa();
+            }
         }
 
-        //getters e setters
+      /*  getters e setters
         public String  getNome(){
                 return nome;
         }        
@@ -49,35 +111,15 @@ public class Pessoa{
         }
         public String getCpf(){
                 return cpf;
-        }
-        public void setCpf(String cpf){
-                this.cpf= cpf;
-        }
+        } */
+       // public void setCpf(String cpf){
+       //         this.cpf= cpf;
+        //}
 
 
 
 
     // Método main
-    public static void main(String[] args) {
-        // Criando um objeto da classe MinhaClasse
-        Pessoa Nicolas = new Pessoa("51936960826","dipirona", "dor de cabeça", "9", "Nicolas");
-
-        // Exibindo os valores dos atributos
-        System.out.println("Nome: " + Nicolas.getNome());
-        System.out.println("ID: " + Nicolas.getCpf());
-
-        // Modificando os valores usando os setters
-        Nicolas.setNome("Nicolas");
-        Nicolas.setCpf("51936960826");
-        Nicolas.setMedicamentos("dipironas");
-        Nicolas.setPressaoArterial("9");
-        
-
-        // Exibindo os valores atualizados
-        System.out.println("Nome atualizado: " + Nicolas.getNome());
-        System.out.println("cpf atualizado: " + Nicolas.getCpf());
-    }
-}
 
 
  //       System.out.println("ddescrição da situação do paciente" +p.getDescricao_paciente());
