922562c83d4901679c622aeed215c995fad52ae9
nicolas <Nicojss1307@gmail.com>
2024-09-26 03:08:27 -0300

    inserindo classe para o banco de daddos

---
+++ inserirPAciente.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+
+public class inserirPAciente {
+    //método para inserir paciente no banco de dados
+
+    public class Passiente(String nome, String cpf, String sintomas,  pressaoArterial);
+
+    
+}
